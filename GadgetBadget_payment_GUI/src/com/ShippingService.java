8441a891180dc41318d001ea5e70daceef95dcf1
AzfaMariyam <azfamariyam@gmail.com>
2021-05-14 18:50:51 +0530

    Changes to service and java class of shipping

--- GadgetBadget_payment_GUI/src/com/ShippingService.java
+++ GadgetBadget_payment_GUI/src/com/ShippingService.java
@@ -1,5 +1,97 @@
 package com;
 
+
+import model.Shipping;
+
+//For REST Service
+import javax.ws.rs.*; 
+import javax.ws.rs.core.MediaType;
+
+//For JSON
+import com.google.gson.*; 
+
+//For XML
+import org.jsoup.*; 
+import org.jsoup.parser.*; 
+import org.jsoup.nodes.Document;
+
+@Path("/Shipment") 
 public class ShippingService {
+	
+	Shipping shipObj = new Shipping();
+	
+	//read shipment details
+	@GET
+	@Path("/")
+	@Produces(MediaType.TEXT_HTML) 
+	public String readShipmentDets() 
+	{ 
+	 return shipObj.readShipmentDetails();
+	} 
+	
+	
+	@POST
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED) 
+	@Produces(MediaType.TEXT_PLAIN) 
+	public String insertShipmentDetails(
+	 @FormParam("firstName") String firstName, 
+	 @FormParam("lastName") String lastName, 
+	 @FormParam("address") String address, 
+	 @FormParam("city") String city,
+	 @FormParam("country") String country, 
+	 @FormParam("zipcode") String zipcode, 
+	 @FormParam("phoneno") String phoneno) 
+	{ 
+		String output = shipObj.insertShipping(firstName, lastName, address, city, country, zipcode, phoneno); 
+	 	return output; 
+	}
+	
+	//update shipment details
+	
+	@PUT
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_JSON) 
+	@Produces(MediaType.TEXT_PLAIN) 
+	public String updateShipment(String shipmentData) 
+	{ 
+		//Convert the input string to a JSON object 
+		 JsonObject shipmentObj = new JsonParser().parse(shipmentData).getAsJsonObject(); 
+		
+		 //Read the values from the JSON object
+		 String shipmentID = shipmentObj.get("shipmentID").getAsString(); 
+		 String firstName = shipmentObj.get("firstName").getAsString(); 
+		 String lastName = shipmentObj.get("lastName").getAsString(); 
+		 String address = shipmentObj.get("address").getAsString(); 
+		 String city = shipmentObj.get("city").getAsString(); 
+		 String country = shipmentObj.get("country").getAsString(); 
+		 String zipcode = shipmentObj.get("zipcode").getAsString(); 
+		 String phoneno = shipmentObj.get("phoneno").getAsString();
+		
+		 String output = shipObj.updateShipment(shipmentID, firstName, lastName, address, city, country, zipcode, phoneno ); 
+		
+		 return output; 
+	}
+	
+	@DELETE
+	@Path("/") 
+	@Consumes(MediaType.APPLICATION_XML) 
+	@Produces(MediaType.TEXT_PLAIN) 
+	public String deleteShipment(String shipmentData) 
+	{ 
+			//Convert the input string to an XML document
+			 Document doc = Jsoup.parse(shipmentData, "", Parser.xmlParser()); 
+			 
+			//Read the value from the element <itemID>
+			 String shipmentID = doc.select("shipmentID").text(); 
+			 String output = shipObj.deleteShipment(shipmentID); 
+			
+			 return output; 
+	}
+
+
+	
+	
+
 
 }
--- GadgetBadget_payment_GUI/src/model/Shipping.java
+++ GadgetBadget_payment_GUI/src/model/Shipping.java
@@ -1,5 +1,230 @@
 package model;
 
+
+import java.sql.*; 
+
 public class Shipping {
+	
+	private Connection connect() 
+	{ 
+		Connection con = null; 
+		try
+		{ 
+			Class.forName("com.mysql.jdbc.Driver"); 
+	 
+			//connect to paymentgb database - DBServer/DBName, username, password 
+			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/paymentgb","root", ""); 
+		} 
+		catch (Exception e) 
+			{e.printStackTrace();} 
+		return con; 
+	 }
+	
+	
+	//insert shipping details
+			public String insertShipping(String firstName, String lastName, String address, String city, String country, String zipcode, String phoneno) 
+			{ 
+				String output = ""; 
+				try{
+					Connection con = connect(); 
+					if (con == null) 
+					{
+						return "Error while connecting to the database for inserting."; 
+					} 
+					
+					// create a prepared statement
+					String query = " insert into shippingdetails (`shipmentID`,`firstName`,`lastName`,`address`,`city`,`country`, `zipcode`,`phoneno`)"
+							+ " values (?, ?, ?, ?, ?, ?, ?, ?)"; 
+					
+					PreparedStatement preparedStmt = con.prepareStatement(query); 
+					// binding values
+					 preparedStmt.setInt(1, 0); 
+					 preparedStmt.setString(2, firstName); 
+					 preparedStmt.setString(3, lastName); 
+					 preparedStmt.setString(4, address); 
+					 preparedStmt.setString(5, city); 
+					 preparedStmt.setString(6, country); 
+					 preparedStmt.setInt(7, Integer.parseInt(zipcode)); 
+					 preparedStmt.setInt(8, Integer.parseInt(phoneno));
+				//	 preparedStmt.setDouble(4, Double.parseDouble(price)); 
+					 
+					 
+					 
+					// execute the statement
+					 preparedStmt.execute(); 
+					
+					 con.close(); 
+					 String newShippment = readShipmentDetails();
+					 output = "{\"status\":\"success\", \"data\": \"" + newShippment + "\"}";
+				 	
+				}
+				catch (Exception e) 
+				{ 
+					 output = "{\"status\":\"error\", \"data\": \"Error while inserting the shipment details.\"}";
+					 System.err.println(e.getMessage()); 
+					 System.out.println(e.getMessage());
+						System.out.println(e);
+						e.printStackTrace();
+					  
+				} 
+				return output; 
+			}
+			
+			
+			public String readShipmentDetails() 
+			{ 
+				 String output = ""; 
+				 try
+				 { 
+					 Connection con = connect(); 
+					 if (con == null) 
+					 {
+						 return "Error while connecting to the database for reading.";  
+					 }
+					 
+					 // Prepare the html table to be displayed
+					 output = "<table border='1'><tr><th>First Name</th><th>Last Name</th>" +
+					 "<th>Address</th>" + 
+					 "<th>City</th>" +
+					 "<th>Country</th><th>Zip Code</th>" +
+					 "<th>Phone number</th></tr>"; 
+					 
+					 String query = "select * from shippingdetails"; 
+					 Statement stmt = con.createStatement(); 
+					 ResultSet rs = stmt.executeQuery(query); 
+
+					// iterate through the rows in the result set
+					 while (rs.next()) 
+					 { 
+						 String shipmentID = Integer.toString(rs.getInt("shipmentID")); 
+						 String firstName = rs.getString("firstName"); 
+						 String lastName = rs.getString("lastName"); 
+						 String address = rs.getString("address"); 
+						 String city = rs.getString("city"); 
+						 String country = rs.getString("country"); 
+						 String zipcode = Integer.toString(rs.getInt("zipcode")); 
+						 String phoneno = Integer.toString(rs.getInt("phoneno")); 
+						 
+						 
+						 // Add into the html table
+						 output += "<tr><td>" + firstName + "</td>"; 
+						 output += "<td>" + lastName + "</td>"; 
+						 output += "<td>" + address + "</td>"; 
+						 output += "<td>" + city + "</td>"; 
+						 output += "<td>" + country + "</td>"; 
+						 output += "<td>" + zipcode + "</td>"; 
+						 output += "<td>" + phoneno + "</td>";
+						 
+						 // buttons
+						 output += "<td><input name='btnUpdate' type='button' value='Update' class='btn btn-secondary'></td>"
+						 + "<td><form method='post' action='shipmentdetails.jsp'>"
+						 + "<input name='btnRemove' type='submit' value='Remove' class='btn btn-danger'>"
+						 + "<input name='itemID' type='hidden' value='" + shipmentID  
+						 + "'>" + "</form></td></tr>"; 
+					 }
+					 
+					 con.close(); 
+					 
+					 // Complete the html table
+					 output += "</table>"; 
+					 
+				 } 
+				 catch (Exception e) 
+				 { 
+					 output = "Error while reading the items."; 
+					 System.err.println(e.getMessage()); 
+				 } 
+				 
+				 return output; 
+				 
+			} 
+			
+			
+			public String updateShipment(String shipmentID, String firstname, String lastname, String address, String city, String country, String zipcode, String phoneno)
+			{ 
+				 String output = ""; 
+				 
+				 try
+				 { 
+					 Connection con = connect(); 
+					 
+					 if (con == null) 
+					 {
+						 return "Error while connecting to the database for updating.";
+					 } 
+					 
+					 // create a prepared statement
+					 String query = "UPDATE shippingdetails SET firstname=?,lastname=?,address=?,city=?,country=?,zipcode=?,phoneno=? WHERE shipmentID =?"; 
+					 
+					 PreparedStatement preparedStmt = con.prepareStatement(query); 
+					 
+					 // binding values
+					 preparedStmt.setString(1, firstname); 
+					 preparedStmt.setString(2, lastname); 
+					 preparedStmt.setString(3, address); 
+					 preparedStmt.setString(4, city); 
+					 preparedStmt.setString(5, country); 
+					 preparedStmt.setInt(6, Integer.parseInt(zipcode) );
+					 preparedStmt.setInt(7, Integer.parseInt(phoneno));
+					 preparedStmt.setInt(8, Integer.parseInt(shipmentID)); 
+					
+					 // execute the statement
+					 preparedStmt.execute(); 
+					 con.close(); 
+					//output = "Updated successfully"; 
+					 String newShippment = readShipmentDetails(); 
+					 output = "{\"status\":\"success\", \"data\": \"" + newShippment + "\"}"; 
+				 } 
+				 catch (Exception e) 
+				 { 
+					 output = "{\"status\":\"error\", \"data\": \"Error while updating the shipment details.\"}"; 
+					 //output = "Error while updating the shipment details."; 
+					 System.err.println(e.getMessage());
+					 System.out.println(e);
+				 } 
+				 
+				 return output; 
+				 
+			} 
+			
+			public String deleteShipment(String shipmentID) 
+			{ 
+				String output = ""; 
+				 try
+				 { 
+					 Connection con = connect(); 
+					 
+					 if (con == null) 
+					 {
+						 return "Error while connecting to the database for deleting."; 
+					 }
+					 
+					 // create a prepared statement
+					 String query = "delete from shippingdetails where shipmentID=?"; 
+					 
+					 PreparedStatement preparedStmt = con.prepareStatement(query); 
+					 
+					 // binding values
+					 preparedStmt.setInt(1, Integer.parseInt(shipmentID)); 
+					 
+					 // execute the statement
+					 preparedStmt.execute(); 
+					 con.close();
+					 String newShippment = readShipmentDetails(); 
+					 output = "{\"status\":\"success\", \"data\": \"" + newShippment + "\"}"; 
+					 //output = "Deleted successfully"; 
+				 } 
+				 catch (Exception e) 
+				 { 
+					 output = "{\"status\":\"error\", \"data\": \"Error while deleting the shipment details.\"}"; 
+					 //output = "Error while deleting the shipment details."; 
+					 System.err.println(e.getMessage()); 
+					 System.out.println(e);
+				 } 
+				 return output; 
+				 
+			}
+			
+		
 
 }
